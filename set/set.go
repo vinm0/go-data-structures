cd0cc9f9345ec559ff40b08fd00ed60a000f6090
Vincent Martinez <vincentem20@gmail.com>
2021-07-21 20:26:56 -0500

    Add set documentation comments

--- set/set.go
+++ set/set.go
@@ -14,27 +14,37 @@ func NewSet() *set {
 	return s
 }
 
+// Returns the size of the set
 func (s *set) Size() int {
 	return len(s.data)
 }
 
+// Adds one or more items to the set
+// Note: Slices should be unpacked.
 func (s *set) Add(items ...interface{}) {
 	for _, item := range items {
 		s.data[item] = struct{}{}
 	}
 }
 
+// Removes one or more items from the set
+// Note: Slices should be unpacked.
 func (s *set) Remove(items ...interface{}) {
 	for _, item := range items {
 		delete(s.data, item)
 	}
 }
 
+// Returns true if item exists in the set.
+// Otherwise, returns false
 func (s *set) Contains(item interface{}) bool {
 	_, ok := s.data[item]
 	return ok
 }
 
+// Returns true if all items exist in the set.
+// Otherwise, returns false.
+// Note: Slices should be unpacked.
 func (s *set) ContainsAll(items ...interface{}) bool {
 	for _, item := range items {
 
@@ -47,6 +57,9 @@ func (s *set) ContainsAll(items ...interface{}) bool {
 	return true
 }
 
+// Returns true if any item provided exists in the set.
+// Returns false if no item exists.
+// Note: Slices should be unpacked.
 func (s *set) ContainsAny(items ...interface{}) bool {
 	for _, item := range items {
 
@@ -59,14 +72,18 @@ func (s *set) ContainsAny(items ...interface{}) bool {
 	return false
 }
 
+// Empties the set
 func (s *set) Clear() {
 	*s = set{}
 }
 
+// Returns true if the set is empty. Otherwise, returns false
 func (s *set) IsEmpty() bool {
 	return len(s.data) == 0
 }
 
+// Returns a slice containing each value in the set.
+// Tip: Minimize calls to ToSlice()
 func (s *set) ToSlice() []interface{} {
 	setSlice := make([]interface{}, 0, len(s.data))
 	for key := range s.data {
