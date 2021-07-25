02568a35e29adbefca05160a8c16492f43687dc8
Vincent Martinez <vincentem20@gmail.com>
2021-07-24 23:16:07 -0500

    Drafting tree.go

---
+++ tree.go
@@ -0,0 +1,99 @@
+package main
+
+type tree struct {
+	root   *treeNode
+	height int
+	size   int
+}
+
+type treeNode struct {
+	value  int
+	left   *treeNode
+	right  *treeNode
+	parent *treeNode
+}
+
+func NewTree() *tree {
+	return &tree{}
+}
+
+func (t *tree) Size() int {
+	return t.size
+}
+
+func (t *tree) Height() int {
+	return t.height
+}
+
+func (t *tree) Root() *treeNode {
+	return t.root
+}
+
+func (n *treeNode) Parent() *treeNode {
+	return n.parent
+}
+
+func (n *treeNode) LeftChild() *treeNode {
+	return n.left
+}
+
+func (n *treeNode) RightChild() *treeNode {
+	return n.right
+}
+
+func (n *treeNode) HasLeft() bool {
+	return n.left != nil
+}
+
+func (n *treeNode) HasRight() bool {
+	return n.right != nil
+}
+
+func (n *treeNode) IsLeaf() bool {
+	return !(n.HasLeft() || n.HasRight())
+}
+
+func (t *tree) Add(val int) (ok bool) {
+	n := &treeNode{value: val}
+
+	if t.root == nil {
+		t.root = n
+		return true
+	}
+
+	ok = appendNode(t.root, n)
+
+	return ok
+}
+
+func (t *tree) Remove(val int) (ok bool) {
+
+	return removeValue(t.root, val)
+}
+
+// TODO: refactor as a generic function to
+// ? Maybe add valType string field to tree, switch methods by type
+func appendNode(curr *treeNode, n *treeNode) (ok bool) {
+
+	if n.value < curr.value {
+		if !curr.HasLeft() {
+			curr.left = n
+			n.parent = curr
+			return true
+		}
+
+		return appendNode(curr.left, n)
+	}
+
+	if !curr.HasRight() {
+		curr.right = n
+		return true
+	}
+
+	return appendNode(curr.right, n)
+}
+
+func removeValue(curr *treeNode, val int) (ok bool) {
+	// TODO
+	return false
+}
