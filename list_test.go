df1fd4d728d33b79ec4e0530f1f95eeb3397798b
Vincent Martinez <vincentem20@gmail.com>
2021-09-11 14:17:26 -0500

    Updated list tests for new list type

--- list_test.go
+++ list_test.go
@@ -11,20 +11,20 @@ func TestPop(t *testing.T) {
 		wantPop interface{}
 		wantLen int
 	}{
-		{*NewList(), nil, 0},
-		{*sampleList(nil), nil, 0},
-		{*sampleList(""), "", 0},
-		{*sampleList(0, "", nil), nil, 2},
-		{*sampleList(0, 1, 2), 2, 2},
-		{*sampleList(0, "two"), "two", 1},
+		{NewList(), nil, 0},
+		{sampleList(nil), nil, 0},
+		{sampleList(""), "", 0},
+		{sampleList(0, "", nil), nil, 2},
+		{sampleList(0, 1, 2), 2, 2},
+		{sampleList(0, "two"), "two", 1},
 	}
 
 	for _, tC := range testCases {
 		testname := fmt.Sprintln(tC.l, ":", tC.wantPop)
 		t.Run(testname, func(t *testing.T) {
 			pop := tC.l.Pop()
-			if len(tC.l.sli) != tC.wantLen {
-				t.Errorf("got len %d, want len %d", len(tC.l.sli), tC.wantLen)
+			if len(tC.l) != tC.wantLen {
+				t.Errorf("got len %d, want len %d", len(tC.l), tC.wantLen)
 			}
 			if pop != tC.wantPop {
 				t.Errorf("got pop %v, want %v", pop, tC.wantPop)
@@ -33,7 +33,7 @@ func TestPop(t *testing.T) {
 	}
 }
 
-func sampleList(args ...interface{}) *list {
+func sampleList(args ...interface{}) list {
 	l := NewList()
 	for _, v := range args {
 		l.Append(v)
