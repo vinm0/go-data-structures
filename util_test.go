c609ca72fb48fa1dd5bc109a36a1d2f57d126e6c
Vincent Martinez <vincentem20@gmail.com>
2021-09-11 16:00:00 -0500

    add test: ValidIndex

--- util_test.go
+++ util_test.go
@@ -30,3 +30,30 @@ func TestHashable(t *testing.T) {
 		})
 	}
 }
+
+func TestValidIndex(t *testing.T) {
+	testCases := []struct {
+		sli   []interface{}
+		index int
+		want  bool
+	}{
+		{[]interface{}{}, 0, false},
+		{[]interface{}{}, -1, false},
+		{[]interface{}{}, 1, false},
+		{[]interface{}{1}, 0, true},
+		{[]interface{}{1}, 1, false},
+		{[]interface{}{1, "two"}, 1, true},
+		{[]interface{}{1, nil}, 1, true},
+		{[]interface{}{1, "two"}, 2, false},
+		{[]interface{}{1, nil}, 2, false},
+	}
+	for _, tC := range testCases {
+		testname := fmt.Sprintf("%v : %d", tC.sli, tC.index)
+		t.Run(testname, func(t *testing.T) {
+			ans := ValidIndex(tC.index, len(tC.sli))
+			if ans != tC.want {
+				t.Errorf("got %t, want %t", ans, tC.want)
+			}
+		})
+	}
+}
