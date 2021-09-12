ff309de3693ab306b9b7e7c42cbe9776e7a688d3
Vincent Martinez <vincentem20@gmail.com>
2021-09-12 14:14:29 -0500

    add test: Insert LinkedList

--- linkedlist_test.go
+++ linkedlist_test.go
@@ -122,3 +122,76 @@ func TestPushFrontLL(t *testing.T) {
 		})
 	}
 }
+
+func TestInsert(t *testing.T) {
+	tests := []struct {
+		ll       linkedlist
+		index    int
+		val      interface{}
+		want     bool
+		wantList linkedlist
+	}{
+		{
+			linkedlist{},
+			0,
+			nil,
+			true,
+			linkedlist{head: &node{nil, nil}, tail: &node{nil, nil}, size: 1},
+		},
+		{
+			linkedlist{head: &node{nil, nil}, tail: &node{nil, nil}, size: 1},
+			0,
+			nil,
+			true,
+			linkedlist{head: &node{nil, nil}, tail: &node{nil, nil}, size: 2},
+		},
+		{
+			linkedlist{head: &node{nil, nil}, tail: &node{nil, nil}, size: 2},
+			1,
+			"three",
+			true,
+			linkedlist{head: &node{nil, &node{"three", &node{nil, nil}}}, tail: &node{nil, nil}, size: 3},
+		},
+		{
+			linkedlist{head: &node{nil, &node{nil, nil}}, tail: &node{nil, nil}, size: 2},
+			-1,
+			"three",
+			false,
+			linkedlist{head: &node{nil, nil}, tail: &node{nil, nil}, size: 2},
+		},
+		{
+			linkedlist{head: &node{nil, &node{nil, nil}}, tail: &node{nil, nil}, size: 2},
+			2,
+			"three",
+			true,
+			linkedlist{head: &node{nil, &node{nil, &node{"three", nil}}}, tail: &node{"three", nil}, size: 3},
+		},
+		{
+			linkedlist{head: &node{nil, &node{nil, nil}}, tail: &node{nil, nil}, size: 2},
+			0,
+			"three",
+			true,
+			linkedlist{head: &node{"three", &node{nil, &node{nil, nil}}}, tail: &node{nil, nil}, size: 3},
+		},
+	}
+
+	for i, tt := range tests {
+		testname := fmt.Sprintf("Linked List PushFront test %d", i)
+		t.Run(testname, func(t *testing.T) {
+			ans := tt.ll.Insert(tt.val, tt.index)
+			if ans != tt.want {
+				t.Errorf("got %v, want %v", ans, tt.want)
+			}
+			if tt.ll.size != tt.wantList.size {
+				t.Errorf("got %v size, want %v size", tt.ll.size, tt.wantList.size)
+			}
+			for i := 0; i < tt.ll.size; i++ {
+				v1 := tt.ll.Index(i)
+				v2 := tt.wantList.Index(i)
+				if v1 != v2 {
+					t.Errorf("got %v at %d, want %v", v1, i, v2)
+				}
+			}
+		})
+	}
+}
